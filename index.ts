e1037b49a05704f9eed9e60179be90750e66956b
Shihyu Ho <methodho@gmail.com>
2026-03-03 10:27:06 +0800

    fix: only export Plugin function from index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,1 @@
 export { CommandInjectPlugin } from "./src/plugin"
-export {
-    aggregateCommandSources,
-    MakefileCommandSource,
-    PackageScriptsCommandSource,
-} from "./src/command-sources"
-export type {
-    CommandInfo,
-    CommandSource,
-    LoadContext,
-    Logger,
-} from "./src/command-sources"
